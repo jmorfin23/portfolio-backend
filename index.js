3980f57ffe25c739d31b539fa72b11107ffd94ae
Jonathan Morfin <jmorfin776@gmail.com>
2020-12-17 21:33:45 -0500

    contact email api route completed

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express'); 
 const app = express(); 
+const cors = require('cors'); 
 const port = 5000; 
 
+app.use(cors()); 
+
+// app.get(route, callback)
 app.get('/', (req, res) => {
     res.send("my first express server!");
 }); 
 
+app.use('/api', require('./routes.js')); 
+
+//app.listen(port, [host], [backlog], [callback]])
 app.listen(port, () => console.log(`server started on port ${port}`));
---
+++ routes.js
@@ -0,0 +1,46 @@
+const express = require('express'); 
+const router = express.Router(); 
+const nodemailer = require('nodemailer'); 
+const bodyParser = require('body-parser'); 
+const jsonParser = bodyParser.json(); 
+require('dotenv').config();
+
+// login to gmail SMTP 
+const contactEmail = nodemailer.createTransport({
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+});
+
+// **** Api Routes **** / 
+
+
+
+// Send contact email 
+router.post('/contact-email', jsonParser, (req, res) => {
+    
+    const { name, email, subject, message } = req.body; 
+    
+    const mail = {
+        from: req.body.name,
+        to: process.env.SMTP_EMAIL,
+        subject: "Contact Form Submission",
+        html: `<p>Name: ${req.body.name}</p>
+               <p>Email: ${req.body.email}</p>
+               <p>Subject: ${req.body.subject}</p>
+               <p>Message: ${req.body.message}</p>`,
+      };
+
+      contactEmail.sendMail(mail, (error) => {
+        if (error) {
+          res.json({ status: "Error try again." });
+        } else {
+          res.json({ status: "Message sent. Thank you." });
+        }
+      });
+}); 
+
+
+module.exports = router; 
