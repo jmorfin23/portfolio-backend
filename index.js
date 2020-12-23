eca9035a31236deabc76dd89f21bddb21bcbe35f
Jonathan Morfin <jmorfin776@gmail.com>
2020-12-23 18:49:10 -0500

    heroku ready

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const app = express(); 
 const cors = require('cors');  
-
+const PORT = process.env.PORT || 5000; 
 app.use(cors()); 
 
 // app.get(route, callback)
@@ -12,4 +12,4 @@ app.get('/', (req, res) => {
 app.use('/api', require('./routes.js')); 
 
 //app.listen(port, [host], [backlog], [callback]])
-app.listen(process.env.PORT || 5000, () => console.log(`server started on port ${port}`));
+app.listen(PORT , () => console.log(`server started on port ${port}`));
