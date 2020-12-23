a14563cb9926912373cc4091b50b854348587dfd
Jonathan Morfin <jmorfin776@gmail.com>
2020-12-23 18:41:09 -0500

    procfile added

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express'); 
 const app = express(); 
-const cors = require('cors'); 
-const port = 5000; 
+const cors = require('cors');  
 
 app.use(cors()); 
 
 // app.get(route, callback)
 app.get('/', (req, res) => {
-    res.send("my first express server!");
+    res.send("My express server!");
 }); 
 
 app.use('/api', require('./routes.js')); 
 
 //app.listen(port, [host], [backlog], [callback]])
-app.listen(port, () => console.log(`server started on port ${port}`));
+app.listen(process.env.PORT || 5000, () => console.log(`server started on port ${port}`));
